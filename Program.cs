916bb1389681c22aa38cd3947bd93507ad2c7b6a
Сергей <seregabur@live.ru>
2022-03-27 21:17:06 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Sequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int beginningSequnce=5;
+
+            for(int i = 0; i < 96; i++)
+            {
+                i = beginningSequnce;
+                Console.WriteLine(beginningSequnce);
+                Console.ReadLine();
+                beginningSequnce = beginningSequnce + 7;
+            }
+        }
+    }
+}
